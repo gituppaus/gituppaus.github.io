50e43ee1a2c5156cd337d7e48f422bfaaa2c22ad
gituppaus <olli.keinanen@myy.haaga-helia.fi>
2024-10-07 11:23:58 +0300

    Add files via upload

---
+++ versio1/script.js
@@ -0,0 +1,9 @@
+// Nykyaikainen let-muuttujan käyttö funktiossa
+function handleClick1() {
+    let message = "Ajanvaraus on nyt vahvistettu.";
+    alert(message);
+}
+// Navigate to palvelut.html when CTA button is clicked
+function handleClick() {
+    window.location.href = 'palvelut.html'; // Redirect to the Palvelut page
+}
